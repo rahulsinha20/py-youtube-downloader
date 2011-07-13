0baedf822d9d162af1e53cbea2057a1b11a14eae
callmerahul20 <callmerahul20@gmail.com>
2011-07-13 11:22:44 +0000

    Added exception check when media player path has a problem FIX: Display settings dialog

--- src/youtube/util/VlcPlayer.py
+++ src/youtube/util/VlcPlayer.py
@@ -13,20 +13,24 @@ class VlcPlayer(object):
     '''
 
 
-    def __init__(self, videoFilePath, vlcPath):
+    def __init__(self,parent, videoFilePath, vlcPath):
         '''
         Constructor - Damn i had lots of trouble using VLC path having several spaces
         and also the library file
         Also See - http://coding.derkeiler.com/Archive/Python/comp.lang.python/2008-03/msg01930.html
         '''
-        fileName = os.path.abspath(videoFilePath)
-        print fileName
-        print 'Playing:', vlcPath + '"' + fileName + '"'
-        vlcExecCommandString = r'"' + vlcPath + ' "'
-        print "VLC Exec String:", vlcExecCommandString
-#        process = subprocess.Popen('c:\\progra~1\\VideoLAN\VLC\\vlc.exe "'+fileName+'"'+' --one-instance --playlist-enqueue', shell=True, stdout=subprocess.PIPE)
-        libraryFileString = r'"' + videoFilePath + '"'
-        print "Library Video Path:", libraryFileString
-        print "Complete Command:", vlcExecCommandString + libraryFileString
-        process = subprocess.Popen(str(vlcExecCommandString) + str(libraryFileString) + ' --one-instance --playlist-enqueue', stdout=subprocess.PIPE)
+        try:
+            fileName = os.path.abspath(videoFilePath)
+            print fileName
+            print 'Playing:', vlcPath + '"' + fileName + '"'
+            vlcExecCommandString = r'"' + vlcPath + ' "'
+            print "VLC Exec String:", vlcExecCommandString
+    #        process = subprocess.Popen('c:\\progra~1\\VideoLAN\VLC\\vlc.exe "'+fileName+'"'+' --one-instance --playlist-enqueue', shell=True, stdout=subprocess.PIPE)
+            libraryFileString = r'"' + videoFilePath + '"'
+            print "Library Video Path:", libraryFileString
+            print "Complete Command:", vlcExecCommandString + libraryFileString
+            process = subprocess.Popen(str(vlcExecCommandString) + str(libraryFileString) + ' --one-instance --playlist-enqueue', stdout=subprocess.PIPE)
         
+        except:
+            #For any exceptions display settings dialog
+            parent.showSettingsDialogSlot()
